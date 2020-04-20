04b097cb4e984768a6d59b240b29742d29efa124
William Mittard <billymittard@ymail.com>
2020-04-20 16:06:12 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -3,4 +3,13 @@ class Cat
   def name=(cats_name)
     @name = cats_name
   end 
+  def name 
+    @name 
+  end 
+  def meow=(cat_meow)
+    @meow = cat_meow
+  end 
+  def meow
+    @meow 
+  end 
 end 
